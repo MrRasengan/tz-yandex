22dc3fa6330d7157205c648d83da7aaccfca1c99
Konstantin <kostik-kostik-12@mail.ru>
2024-10-14 20:27:23 +0300

    add works

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ let itemsInCart = 0;
 
 products.forEach(product => {
     product.addEventListener('dragstart', dragStart);
+    product.addEventListener('touchstart', touchStart);
 });
 
+// События для корзины
 cart.addEventListener('dragover', dragOver);
 cart.addEventListener('drop', drop);
+cart.addEventListener('touchmove', touchMove);
+cart.addEventListener('touchend', touchEnd);
 
 function dragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.outerHTML); // Сохраняем HTML продукта
+}
+
+function touchStart(event) {
+    const productHTML = event.target.outerHTML;
+    event.dataTransfer = event.dataTransfer || {}; // Создаем объект dataTransfer для поддержки touch
+    event.dataTransfer.setData('text/plain', productHTML);
 }
 
 function dragOver(event) {
@@ -22,8 +32,21 @@ function dragOver(event) {
 function drop(event) {
     event.preventDefault();
     const productHTML = event.dataTransfer.getData('text/plain');
+    addToCart(productHTML);
+}
+
+function touchMove(event) {
+    event.preventDefault(); // Предотвращаем прокрутку
+}
+
+function touchEnd(event) {
+    const productHTML = event.dataTransfer.getData('text/plain');
+    addToCart(productHTML);
+}
+
+function addToCart(productHTML) {
     // Создаем новый элемент из HTML
-    const newItem = document.createElement('prod');
+    const newItem = document.createElement('div'); // Измените на 'div' вместо 'prod'
     newItem.innerHTML = productHTML;
     // Добавляем новый элемент в контейнер корзины
     cartItemsContainer.appendChild(newItem.firstChild);
@@ -33,14 +56,15 @@ function drop(event) {
         animateButton(checkoutButton);
     }
 }
+
 function animateButton(button) {
-  button.classList.add('animate');
-  setInterval(() => {
-      button.classList.toggle('highlight');
-  }, 500);
+    button.classList.add('animate');
+    setInterval(() => {
+        button.classList.toggle('highlight');
+    }, 500);
 }
 
 // Обработчик клика для кнопки "Оплатить корзину"
 checkoutButton.addEventListener('click', () => {
-  window.location.href = 'https://lavka.yandex.ru/';
+    window.location.href = 'https://lavka.yandex.ru/';
 });
